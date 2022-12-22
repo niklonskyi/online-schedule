822ad06586d20bc9e2c117288d542d45b3a54725
niklonskyi <niklonskyi@icloud.com>
2022-12-22 13:42:18 +0200

    Added Footer component

---
+++ my-app/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <footer class="d-flex fixed-bottom justify-content-center py-3">
+      <h5>Made by Niklonskyi</h5>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
